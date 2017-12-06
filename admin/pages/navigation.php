dfcf8a17de0a0c1dd902838fb05a981cb80c9031
JESSYV96 <c.j91350@gmail.com>
2017-12-06 22:20:59 +0100

    ticket b001

--- admin/pages/navigation.php
+++ admin/pages/navigation.php
@@ -1,3 +1,11 @@
+<style>
+  .button {
+    background-color: #a64dff;
+    color: white;
+  }
+
+</style>
+
 <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -11,12 +19,11 @@
                 <a class="navbar-brand" href="index.php">UAFF Admin</a>
             </div>
             <!-- Top Menu Items -->
+            <a href="../../gv/index.php" target="_blank"><button class="btn button" type="button">Voir le site</button></a>
+
             <ul class="nav navbar-right top-nav ">
-                
-               
                 <li class="dropdown">
                     <a href="logout.php" class="logout"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
-                    
                 </li>
             </ul>
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
@@ -27,7 +34,7 @@
                     <li>
                         <a href="matchs.php"><i class="fa fa-fw fa-calendar"></i> Matchs</a>
                     </li>
-                    
+
                     <li>
                         <a href="classements.php"><i class="fa fa-fw fa-newspaper-o"></i> Classements</a>
                     </li>
@@ -44,7 +51,7 @@
                     <li>
                         <a href="rebours.php"><i class="fa fa-fw fa-eur"></i> Rebourss</a>
                     </li>
-                    
+
                 </ul>
             </div>
 
